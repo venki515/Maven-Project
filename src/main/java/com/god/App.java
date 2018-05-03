c8c5dcf209dc418dd0c8e9e76554741b19985475
Venki <venki.linux7@gmail.com>
2018-05-03 11:58:48 +0530

    Maven

---
+++ src/main/java/com/god/App.java
@@ -0,0 +1,13 @@
+package com.god;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
